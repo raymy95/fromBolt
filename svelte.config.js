14d5778a4ba2454ed60d4275adb17dd92cf3820c
raymy95 <raymy95@gmail.com>
2025-03-27 20:15:49 +0100

    static redirect

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        // Ignore all 404s
+        if (message.includes('Not found')) {
+          return;
+        }
+        throw new Error(message);
+      }
+    }
   },
   preprocess: vitePreprocess()
 };
